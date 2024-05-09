94ad259376c1173bf291ecdbb26732654658f39e
gpcc <gpcc@cin.ufpe.br>
2024-05-09 16:11:46 -0300

    light
    codigo para adicionar ponto de iluminacao

---
+++ point_light.py
@@ -0,0 +1,28 @@
+class Light:
+    """
+    Representa uma fonte de luz em uma cena de renderizacao.
+
+    Atributos:
+        pos (Vector): A posicao da fonte de luz.
+        color (tuple): A cor da luz, representada como uma tupla (R, G, B).
+    """
+
+    def __init__(self, pos, color):
+        """
+        Inicializa uma instancia da fonte de luz com uma posicao e cor especificadas.
+
+        Args:
+            pos (Vector): A posicao da fonte de luz.
+            color (tuple): A cor da luz.
+        """
+        self.pos = pos
+        self.color = color
+
+    def get_source(self):
+        """
+        Retorna a posicao da fonte de luz.
+
+        Returns:
+            Vector: A posicao da fonte de luz.
+        """
+        return self.pos
