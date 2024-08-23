8761b1cf5838068195a1022aed9ed0bc5a3d68f4
Cafeko <caiofgs2329@gmail.com>
2024-08-23 17:17:27 -0300

    Refactor: Curve variables names

--- curve.py
+++ curve.py
@@ -2,21 +2,21 @@ import math_stuff as maths
 from point import *
 
 class Curve:
-    def __init__(self, control_points: list, curve_complexity: int):
+    def __init__(self, control_points: list, curve_complexity: int = 1):
         self.control_points = control_points
         self.points = self.generate_curve_points(curve_complexity)
 
     def calculate_bezier_curve(self, t : float):
         """Calcula o ponto na curva, de acordo com o t recebido."""
-        resultado = Point(0, 0, 0)
+        curve_point = Point(0, 0, 0)
         n = len(self.control_points)-1
         for i in range(len(self.control_points)):
-            B = self.calculate_bernstein_polynomial(i, n, t)
+            B = self.calculate_bernstein_polynomial(n, i, t)
             point = self.control_points[i]
-            resultado += point*B
-        return resultado
+            curve_point += point*B
+        return curve_point
     
-    def calculate_bernstein_polynomial(self, i : int, n : int, t: float):
+    def calculate_bernstein_polynomial(self, n : int, i : int, t: float):
         """Calcula o polinomio de Bernstein de acordo com os parametros recebidos."""
         binomial_coef = maths.binomial_coefficient(n, i)
         return binomial_coef*((1-t)**(n-i))*(t**i)
